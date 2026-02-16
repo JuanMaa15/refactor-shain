54a6ef2f11a06d415d8d9096f2159971ed489f79
Juan Manuel <juanmanuel.41595@gmail.com>
2026-02-16 13:30:27 -0500

    feat: agrupar e importar variables globales configuradas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import {
+  app,
+  cloudinaryConfig,
+  database,
+  email,
+  jwtConfig,
+} from './config/globals.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [app, database, jwtConfig, cloudinaryConfig, email],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
