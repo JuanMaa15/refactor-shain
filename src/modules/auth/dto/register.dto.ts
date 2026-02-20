f336ee63f4ece6f9c19d6ca06d60753ecaa0eb0a
Juan Manuel <juanmanuel.41595@gmail.com>
2026-02-19 21:24:11 -0500

    feat: dto register

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -4,8 +4,17 @@ import {
   NoWhitespaces,
 } from '@/common/validators/custom-validators';
 import { ValidationMessages } from '@/common/validators/validation-messages';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { UserRole } from '@/generated/prisma/enums';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterDto {
   @ApiProperty({
@@ -49,4 +58,59 @@ export class RegisterDto {
   @IsEmail({}, ValidationMessages.isEmail())
   @IsNotEmpty(ValidationMessages.isNotEmpty('email'))
   email: string;
+
+  @ApiProperty({
+    description:
+      'Contraseña (minimo 8 caracteres, debe incluir letras y números)',
+    example: 'SecurePassword123',
+    minLength: 8,
+  })
+  @IsString(ValidationMessages.isString('contraseña'))
+  @IsNotEmpty(ValidationMessages.isNotEmpty('contraseña'))
+  @MinLength(8, ValidationMessages.minLength('contraseña', 8))
+  @Matches(
+    /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/,
+    ValidationMessages.strongPasswordFormat(),
+  )
+  password: string;
+
+  @ApiProperty({
+    description: 'Confirmación de la contraseñas',
+    example: 'SecurePassword123',
+    minLength: 8,
+  })
+  @IsString(ValidationMessages.isString('Confirmación contraseña'))
+  @IsNotEmpty(ValidationMessages.isNotEmpty('Confirmación contraseña'))
+  @MinLength(8, ValidationMessages.minLength('Confirmación contraseña', 8))
+  @Matches(
+    /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/,
+    ValidationMessages.strongPasswordFormat(),
+  )
+  confirmPassword: string;
+
+  @ApiProperty({
+    description: 'Rol del usuario',
+    example: UserRole.BUSINESS_OWNER,
+  })
+  @IsEnum(UserRole, ValidationMessages.isEnum('role'))
+  @IsNotEmpty(ValidationMessages.isNotEmpty('role'))
+  role: string;
+
+  @ApiPropertyOptional({
+    description: 'Número de teléfono (opcional)',
+    example: '+573001234567',
+  })
+  @IsOptional()
+  @IsStringTrimmed(ValidationMessages.isString('teléfono'))
+  @Matches(/^(\+)?[0-9]{10,15}$/, ValidationMessages.invalidFormat('teléfono'))
+  phone?: string;
+
+  @ApiPropertyOptional({
+    description:
+      'Código de negocio (OBLIGATORIO si el rol es SERVICE_PROVIDER)',
+    example: 'd4f7a3b9c2e1',
+  })
+  @IsOptional()
+  @IsStringTrimmed(ValidationMessages.isString('código de negocio'))
+  businessCode?: string;
 }
