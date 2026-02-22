c2745abb0fdde3fe532b4c6d4833c22b4d321910
Juan Manuel <juanmanuel.41595@gmail.com>
2026-02-21 22:18:18 -0500

    feat: index agrupacion dtos

---
+++ src/modules/auth/dto/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Barrel export para Auth DTOs
+ *
+ * Facilita los imports:
+ * import { RegisterDto, LoginDto } from './dto';
+ */
+
+export { RegisterDto } from './register.dto';
+export { LoginDto } from './login.dto';
+export { ForgotPasswordDto } from './forgot-password.dto';
+export { ResetPasswordDto } from './reset-password.dto';
+export { ChangePasswordDto } from './change-password.dto';
+export { RefreshTokenDto } from './refresh-token.dto';
