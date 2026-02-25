49f6d84cc8f59eacd7a94d8598cfed5c39dd4f55
JuanMa <juanmanuel.41595@gmail.com>
2026-02-25 11:38:00 -0500

    feat(auth): crear guards

---
+++ src/common/guards/jwt-auth.guard.ts
@@ -0,0 +1,29 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  /**
+   * Determina si la request puede activar el guard
+   *
+   * @param context - Contexto de ejecución de NestJS
+   * @returns true si la ruta requiere autenticación, false si es pública
+   */
+  canActivate(context: ExecutionContext) {
+    //Verificar  si la ruta tiene el decorador @Public()
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return false;
+    }
+    return super.canActivate(context);
+  }
+}
